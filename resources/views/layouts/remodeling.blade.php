3e0cee51057eb8e72c5830c1404732f9c3e23789
Tramaine Jackson <tjackson@sicom.com>
2019-11-11 09:03:39 -0500

    Removed mailgun credentials from config/services.php

--- resources/views/layouts/remodeling.blade.php
+++ resources/views/layouts/remodeling.blade.php
@@ -216,7 +216,7 @@
 	
 	<!-- SCRIPTS -->
 	<!-- JQuery -->
-	<script type="text/javascript" src="/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="/js/jquery-3.4.1.min.js"></script>
 	<!-- Bootstrap tooltips -->
 	<script type="text/javascript" src="/js/popper.min.js"></script>
 	<!-- Bootstrap core JavaScript -->
