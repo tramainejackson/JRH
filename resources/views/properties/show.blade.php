60e0837ff589119ff05842187cde12e5d1dda80a
Tramaine Jackson <tjackson@sicom.com>
2021-02-18 16:10:19 -0500

    Updated style for properties pages

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -1,8 +1,4 @@
 @extends('layouts.app')
-@if(Auth::check())
-@else
-@section('addt_style')
-@endsection
 
 @section('content')
 
@@ -14,18 +10,30 @@
 
 			<div class="col-12">
 
-				<h4 class="propertyItem">{{ $property->address }}&nbsp;{{ $property->city }}&nbsp;{{ $property->state }},&nbsp;{{ $property->zip }}</h4>
-				<h5 class="propertyItem">
-					<i class="fa fa-bed" aria-hidden="true"></i>&nbsp;<em>Beds:</em>&nbsp;{{ $property->bed }}&nbsp;&nbsp;
-					<i class="fa fa-bathtub" aria-hidden="true"></i>&nbsp;<em>Baths:&nbsp;</em>{{ $property->bath }}
+				@if($property->sale == 'rent')
+					<h5 class="propertyItem green-text text-center mb-0">{{ $property->price != null ? '$' . $property->price : 'Call for Pricing' }}&nbsp;/per month</h5>
+					<h5 class="text-center mt-0 mb-3"><span class="font-small font-italic">{{ $property->move_in_cost != null ? '$' . $property->move_in_cost . ' /total move in cost' : 'Call for total move in cost' }}</span>&nbsp;&nbsp;<i class="fa fa-info-circle deep-orange-text" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="This is total price it will cost to move in. Normally includes, first and last rent and a security deposit."></i></h5>
+				@endif
+
+				<h1 class="propertyItem d-flex flex-column align-items-center my-2">
+					<span>{{ $property->address }}&nbsp;</span>
+					<span>{{ $property->city }}&nbsp;{{ $property->state }},&nbsp;</span>
+					<span>{{ $property->zip }}</span>
+				</h1>
+
+				<h5 class="propertyItem d-flex align-items-center justify-content-around justify-content-md-center my-2">
+					<span class="indigo-text mr-md-2"><i class="fa fa-bed" aria-hidden="true"></i>&nbsp;Beds:&nbsp;{{ $property->bed }}&nbsp;&nbsp;</span>
+					<span class="deep-purple-text ml-md-2"><i class="fas fa-bath"></i>&nbsp;Baths:&nbsp;{{ $property->bath }}</span>
 				</h5>
 
 				<h5 class="propertyItem">
 
 					@if($property->sale == 'rent')
-						<h5 class="propertyItem">{{ $property->price != null ? '$' . $property->price : 'Call for Pricing' }}&nbsp;/per month</h5>
-						<h5>{{ $property->move_in_cost != null ? '$' . $property->move_in_cost . ' /total move in cost' : 'Call for total move in cost' }}&nbsp;&nbsp;<i class="fa fa-info-circle deep-orange-text" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="This is total price it will cost to move in. Normally includes, first and last rent and a security deposit."></i></h5>
-						<h5>Tenant Responsible For:
+
+						<h5 class="text-center pt-4">Tenant Responsible For:</h5>
+
+						<div class="d-flex align-items-center justify-content-around">
+
 							@if($property->included_utl != null)
 								@if(substr_count($property->included_utl, 'electricity') >= 1)
 									<div class="d-inline px-2"><i class="fa fa-bolt amber-text" aria-hidden="true"></i>&nbsp;Electricity</div>
@@ -41,9 +49,9 @@
 							@else
 								&nbsp;Tenant Not Responsible For Any Utilities
 							@endif
-						</h5>
+						</div>
 
-						@if($property->requirements)
+						@if($property->requirements->isNotEmpty())
 							<div class="">
 								<h5 class=""><u>Property Requirements:</u></h5>
 								<ol class="">
@@ -59,9 +67,9 @@
 
 			<div class="col-12">
 				<div class="">
-					<h2 class="text-center pt-5">{{ $property->title }}</h2>
+					{{--<h2 class="text-center pt-5">{{ $property->title }}</h2>--}}
 
-					<h3><i><u>Description:</u></i></h3>
+					<h3 class="pt-4 mb-0 text-underline font-italic">Description:</h3>
 					<p class="text-justify pb-4">{{ $property->description }}</p>
 
 					@php $dt = new Carbon\Carbon($property->available_date); @endphp
@@ -81,7 +89,9 @@
 			</div>
 
 			<div class="col-12 propertyImgGallery my-4">
+
 				@if($images->isNotEmpty())
+
 					<div id="mdb-lightbox-ui"></div>
 
 					<!--Full width lightbox-->
@@ -90,21 +100,10 @@
 						<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 align-items-center justify-content-center">
 
 							@foreach($images as $image)
-								@php
-									if(file_exists(str_ireplace('public', 'storage', $image->path))) {
-
-										$image_path = str_ireplace('public/images', 'storage/images/sm', asset($image->path));
-
-									} else {
-
-										$image_path = '/images/no_image_lg.png';
-
-									}
-								@endphp
 
 								<figure class="col text-center my-1">
-									<a href="{{ $image_path }}" class="" data-size="1500x{{ isset($image->lg_height) && $image->lg_height != null ? $image->lg_height : '1500' }}">
-										<img src="{{ $image_path }}" class="img-fluid" />
+									<a href="{{ asset($image->path) }}" class="" data-size="1500x{{ isset($image->lg_height) && $image->lg_height != null ? $image->lg_height : '1500' }}">
+										<img src="{{ asset($image->path) }}" class="img-fluid" style="max-height: {{ isset($image->sm_height) && $image->sm_height != null ? $image->sm_height . 'px' : '500px' }}" />
 									</a>
 								</figure>
 
@@ -120,8 +119,4 @@
 			<h1 class="col text-hide my-3" style="border:1px solid #787878 !important">Hidden Text</h1>
 		</div>
 	</div>
-@endsection
-@endif
-
-@section('footer')
 @endsection
